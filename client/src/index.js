db5bd1b99898bef2463b02e74694320d6d8f6aef
Alexandra Hurst <82131699+AlNHurst@users.noreply.github.com>
2021-07-24 12:16:55 -0400

    created modal and added reservation form with inputs

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
 
 ReactDOM.render(
   <React.StrictMode>
--- client/src/pages/TourPackage.js
+++ client/src/pages/TourPackage.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import ReservationForm from '../components/ReservationForm';
+import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
 
 // Import the `useParams()` hook
 import { useParams } from 'react-router-dom';
@@ -6,28 +10,110 @@ import { useQuery } from '@apollo/client';
 
 import { QUERY_TOUR } from '../utils/queries';
 
-const TourPackage = () => {
-  const { tourId } = useParams();
+Modal.setAppElement('#root');
 
+const TourPackage = () => {
+  // query tour by id and show details on page
+  const { tourId } = useParams();
   const { loading, data } = useQuery(QUERY_TOUR, {
     variables: { id: tourId },
   });
-
   const tour = data?.tourPackage || {};
+
+  // Modal functions to open and close
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const openModal = () => {
+    return setIsOpen(true);
+  }
+  const closeModal = () => {
+    return setIsOpen(false);
+  }
+  const onRequestClose = () => {
+    return setIsOpen(false);
+  }
+
+  // calendar component function within modal 
+  const [selectionDate, setSelectionDate] = useState(new Date());
+  const dateChange = selectionDate => {
+    setSelectionDate(selectionDate);
+  }
 
   if (loading) {
     return <div>Loading...</div>;
   }
   return (
     <>
-      <h1>Tour Name: {tour.name} </h1>
-      <p>{tour.description}</p>
-      <ul>
-        <li>Adult Price: ${tour.adultPrice}</li>
-        <li>Child Price: ${tour.childPrice}</li>
-        <li>Duration: {tour.duration} hours</li>
-        <li>Departure Time: {tour.departureTime}</li>
-      </ul>
+      <div>
+        <h1>Tour Name: {tour.name} </h1>
+        <p>{tour.description}</p>
+        <ul>
+          <li>Adult Price: ${tour.adultPrice}</li>
+          <li>Child Price: ${tour.childPrice}</li>
+          <li>Duration: {tour.duration} hours</li>
+          <li>Departure Time: {tour.departureTime}</li>
+        </ul>
+        <button onClick={openModal}>
+          Make Reservation
+        </button>
+      </div>
+
+      {/* Reservation Modal */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={onRequestClose}>
+        <div className="modal-body">
+          <button onClick={closeModal}>
+            &larr; Go Back to Reservation Details
+          </button>
+          <div>
+            <div className="card">
+              <div className="card-body">
+                <h1>You're booking {tour.name}</h1>
+                <h4>Contact Information</h4>
+                <form action="">
+
+                  <div className="field">
+                    <div className="control">
+                      <input type="text" placeholder="Full name" className="input" name="name" required />
+                    </div>
+                  </div>
+
+                  <div className="field">
+                    <div className="control">
+                      <input type="text" placeholder="Phone number" className="input" name="phone" required />
+                    </div>
+                  </div>
+
+                  <div className="field">
+                    <div className="control">
+                      <input type="text" placeholder="Email address" className="input" name="email" required />
+                    </div>
+                  </div>
+
+                  <div className="field">
+                    <button
+                      className="button is-dark"
+                      type="submit">
+                      Complete and Pay
+                    </button>
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </div>
+
+
+
+
+          <Calendar
+            activeStartDate={selectionDate}
+            onChange={dateChange}
+            value={selectionDate}
+          >
+          </Calendar>
+        </div>
+      </Modal>
     </>
   );
 };
