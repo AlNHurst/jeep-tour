e297a9eecbf5e8c75330b54e6f810863025e5d8b
Jess Greene <jess.greene9@gmail.com>
2021-07-27 22:24:25 -0400

    added if statement for map to display reviews

--- client/src/components/TourReviews/index.js
+++ client/src/components/TourReviews/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_USERS } from '../../utils/queries';
-import './style.css';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_USERS } from "../../utils/queries";
+import "./style.css";
 
-import { Card } from 'react-bootstrap';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import { Card } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const TourReviews = () => {
   const { loading, error, data } = useQuery(QUERY_USERS);
-  if (loading) return <div>'Loading...';</div>
+  if (loading) return <div>'Loading...';</div>;
   if (error) return `Error! ${error.message}`;
 
   const usersList = data?.users || [];
@@ -18,26 +18,27 @@ const TourReviews = () => {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 5,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
+
 
   return (
     <Carousel responsive={responsive}>
-      <div className="tourReviews-container">
-        {usersList.map((user) => {
+      <div className="tourReviews-container">{usersList.map((user) => {
+        if (user.review) {
           return (
             <div className="tourReviews-mainbox">
               <div className="tourReviews-imgbox">
@@ -46,12 +47,12 @@ const TourReviews = () => {
               <div className="tourReviews-info">
                 <h4>{user.username}</h4>
                 {/* rating */}
-                {/* <p>{user.review.comment}</p> */}
+                <p>{user.review.comment}</p>
               </div>
             </div>
           );
-        })}
-      </div>
+        }
+      })}</div>
     </Carousel>
   );
 };
